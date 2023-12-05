6777c8bf96fab0266915d8f0938fbda7cf64f71e
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-12-05 23:36:44 +0530

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar/Navbar";
 function App() {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
