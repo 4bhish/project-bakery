89275bccd30a28035e963cebddee357c59a4e63e
Abhishek Hadimani <abhihadimani143@gmail.com>
2023-12-08 21:42:49 +0530

    Refactor Navbar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "../footer/footer.css";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <p className="footer-text">footer text here</p>
+      <p className="footer-text">© 2024 Sweet moments, baked daily at <Link to='/' className="footer-link">FRIENDS BAKERY</Link>.</p>
     </div>
   );
 };
