6bca767908f534d0c142248a9d8208f812f1e951
Abhishek Hadimani <abhihadimani143@gmail.com>
2023-12-08 19:07:22 +0530

    Update Nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "../navbar/navbar.css";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="Navbar">
       <h1 className="nav-h1">Site Name</h1>
       <ul className="nav-list">
-        <li>Home</li>
-        <li>About</li>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>About</NavLink>
+        </li>
         <li>Product</li>
       </ul>
     </div>
